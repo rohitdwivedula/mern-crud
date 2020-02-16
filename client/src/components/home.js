ad461a523cf00d22fe1fed614248c4d01e79d292
rohitdwivedula@gmail.com <rohitdwivedula@gmail.com>
2020-02-16 16:50:40 +0530

    Minor: Change heading

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     return(
       <div className="home-wrap">
         <div className="landing">
-          <h1 className="home-title">Movie Manager</h1>
+          <h1 className="home-title">Movie Database</h1>
             <div className="land-desc col-lg-6 col-md-7">
               A simple app to help you: create your first MERN app, understand how code is organized for webapps like these, and perhaps make a few changes of your own as well too! <a href="/auth/google">Click here</a> to login with Google.
             </div>
